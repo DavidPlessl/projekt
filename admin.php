cae344c5f6b631ef5f367085734e08055453d0e0
SamuelSchorratz <schorratz.samuel@hakspittal.at>
2024-02-18 20:35:41 +0100

    Aktualisierung Adminseite

--- admin.php
+++ admin.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+    <h1>Herzlich Willkommen auf der Adminseite der Freiwilligen Feuerwehr St. Schmorrel</h1><hr>
+
+    <h3>Hier finden sie unsere letzten Einsätze, welche Sie entweder BESTÄTIGEN oder LÖSCHEN können:</h3><hr>
+
 <?php
 
-    require_once('dbConnection.php');
+    require_once('dbconnection.php');
 
     try {
         $statement = $pdo->prepare("SELECT * FROM einsaetze");
@@ -8,38 +20,88 @@
         $statement->execute();
 
         echo "<table>";
-        echo "<hr>";
 
         if ($statement->rowCount() > 0) {
             while ($zeile = $statement->fetch()) {
                 echo "<tr>" .
-                    "<td style='font-weight: bold;'>" . $zeile['datum'] . "</td>" .
-                    "<td style='font-weight: bold;'>" . $zeile['stichwort'] . "</td>" .
-                    "<td style='font-weight: bold; >" . $zeile['einsatzart'] . "</td>" .
-                    "<td style='font-weight: bold; >" . $zeile['einsatzort'] . "</td>" .
-                    "<td style='font-weight: bold; >" . $zeile['fahrzeuge'] . "</td>" .
-                    "<td style='font-weight: bold; >" . $zeile['weitere_kraefte'] . "</td>" .
-                    "<td style='font-weight: bold; >" . $zeile['beschreibung'] . "</td>" .
-                    "<td style='font-weight: bold; >" . $zeile['erstellt_von'] . "</td>" .
-                    "<td><a href='eintragloeschen.php?datum=$zeile[datum]&stichwort=$zeile[stichwort]
-                                                                    &einsatzart=$zeile[einsatzart]&einsatzort=$zeile[einsatzort]
-                                                                    &fahrzeuge=$zeile[fahrzeuge]&weitere_kraefte=$zeile[weitere_kraefte]
-                                                                    &beschreibung=$zeile[beschreibung]&erstellt_von=$zeile[erstellt_von]
-                                                                    >Löschen</a></td>";
-                if($zeile['bestaetigt'] == 0){
-                    echo "<td><a href='bestaetigt.php?id=$zeile[e_id]&datum=$zeile[datum]&stichwort=$zeile[stichwort]
-                    &einsatzart=$zeile[einsatzart]&einsatzort=$zeile[einsatzort]
-                    &fahrzeuge=$zeile[fahrzeuge]&weitere_kraefte=$zeile[weitere_kraefte]
-                    &beschreibung=$zeile[beschreibung]&erstellt_von=$zeile[erstellt_von]
-                    >Bestätigen</a></tr>";
+                    "<th>Datum</th>" . "<th>Stichwort</th>" . "<th>Einsatzart</th>" . 
+                    "<th>Einsatzort</th>" . "<th>Fahrzeuge</th>" . "<th>Weitere Kräfte</th>" .
+                    "<th>Erstellt von</th>" .
+                    "</tr>" . 
+                    "<tr>" .
+                    "<td>" . $zeile['datum'] . "</td>" .
+                    "<td>" . $zeile['stichwort'] . "</td>" .
+                    "<td>" . $zeile['einsatzart'] . "</td>" .
+                    "<td>" . $zeile['einsatzort'] . "</td>" .
+                    "<td>" . $zeile['fahrzeuge'] . "</td>" .
+                    "<td>" . $zeile['weitere_kraefte'] . "</td>" .
+                    //"<td>" . $zeile['beschreibung'] . "</td>" .
+                    "<td>" . $zeile['erstellt_von'] . "</td>" .
+
+                    "<td>" . "<a href='loeschen.php?id=$zeile[E_ID]&datum=$zeile[datum]&stichwort=$zeile[stichwort]&einsatzart=$zeile[einsatzart]&einsatzort=$zeile[einsatzort]&fahrzeuge=$zeile[fahrzeuge]&weitere_kraefte=$zeile[weitere_kraefte]&beschreibung=$zeile[beschreibung]&erstellt_von=$zeile[erstellt_von]&bestaetigt=$zeile[bestaetigt]'>Löschen</a>" . "</td>";
+               
+               
+                if($zeile['bestaetigt'] == 0) {
+
+                    echo "<td>". "<a href='bestaetigen.php?id=$zeile[E_ID]&datum=$zeile[datum]&stichwort=$zeile[stichwort]&einsatzart=$zeile[einsatzart]&einsatzort=$zeile[einsatzort]&fahrzeuge=$zeile[fahrzeuge]&weitere_kraefte=$zeile[weitere_kraefte]&beschreibung=$zeile[beschreibung]&erstellt_von=$zeile[erstellt_von]&bestaetigt=$zeile[bestaetigt]'>Bestätigen</a>" . "</tr>" .
+                        "</tr>";
                 }
             }
         }
         echo "</table>";
+        echo "<hr><hr>";
+
     } catch (PDOException $ex) {
         die("Fehler beim Ausgeben der Datenbank! " . $ex->getMessage());
     }
-    
+
+?>
+
+<h3>Hier finden sie unsere Aktivitäten, welche Sie entweder BESTÄTIGEN oder LÖSCHEN können:</h3><hr>
+
+<?php
+    require_once('dbconnection.php');
+
+    try {
+        $statement = $pdo->prepare("SELECT * FROM aktivitaeten");
+
+        $statement->execute();
+
+        echo "<table>";
+
+        if ($statement->rowCount() > 0) {
+            while ($zeile = $statement->fetch()) {
+
+                echo "<tr>" .
+                    "<th>Datum</th>" . "<th>Aktivität</th>" . "<th>Ort</th>" . 
+                    "<th>Erstellt von</th>" . 
+                    "</tr>" . 
+                    "<tr>" .
+                    "<td>" . $zeile['datum'] . "</td>" .
+                    "<td>" . $zeile['aktivitaet'] . "</td>" .
+                    "<td>" . $zeile['ort'] . "</td>" .
+                    "<td>" . $zeile['erstellt_von'] . "</td>" .
+
+                    "<td>" . "<a href='loeschen.php?A_ID=$zeile[A_ID]&datum=$zeile[datum]&aktivitaet=$zeile[aktivitaet]&ort=$zeile[ort]&beschreibung=$zeile[beschreibung]&erstellt_von=$zeile[erstellt_von]&bestaetigt=$zeile[bestaetigt]'>Löschen</a>" . "</td>";
+
+                if($zeile['bestaetigt'] == 0) {
+
+                    echo "<td>". "<a href='bestaetigen.php?A_ID=$zeile[A_ID]&datum=$zeile[datum]&aktivitaet=$zeile[aktivitaet]&ort=$zeile[ort]&beschreibung=$zeile[beschreibung]&erstellt_von=$zeile[erstellt_von]&bestaetigt=$zeile[bestaetigt]'>Bestätigen</a>" . "</tr>" .
+                        "</tr>";
+                }    
+
+            }
+
+        }
+
+        echo "</table>";
+
+    } catch (PDOException $ex) {
+        die("Fehler beim Ausgeben der Datenbank! " . $ex->getMessage());
+    }
 
 
-    ?>
+?>
+
+</body>
+</html>
