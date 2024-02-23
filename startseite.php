f4d65d64953ac6320f3389005c52b905aedb0ed0
Sante1007 <santner.dominik1@hakspittal.at>
2024-02-23 20:49:59 +0100

    zu Startseite unbenant

--- startseite.php
+++ startseite.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Ihre Webseite</title>
+  <title>Startseite</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -259,7 +259,7 @@
       </a><br><br>
       <p>Facebook</p>
       </div>
-      
+
     </form>
   </footer>
 
