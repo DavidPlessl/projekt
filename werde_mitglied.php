7de663003c5be2c528de2325a0ab6a6be0fcfe05
Sante1007 <santner.dominik1@hakspittal.at>
2024-02-26 11:34:02 +0100

    zurück

--- werde_mitglied.php
+++ werde_mitglied.php
@@ -8,6 +8,7 @@
 <body>
 
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
@@ -23,7 +24,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Optional: Weiterleitung nach dem Versenden der E-Mail
     header("Location: danke.html");
     exit();
-}
+    
+}else if (isset($_POST['zurueck'])) {
+
+    //weiterleiten
+    $pfad1 = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/startseite.php';
+    header('Location: ' . $pfad1);
+}  
 ?>
 
 <h1>Lust Mitglied zu werden?</h1>
@@ -31,17 +38,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 
     <label for="name">Ihr Name:</label><br><br>
-    <input type="text" id="name" name="name" placeholder="Ihr Name" required><br><br>
+    <input type="text" id="name" name="name" placeholder="Ihr Name" ><br><br>
 
     <label for="email">Ihre Email:</label><br><br>
-    <input type="email" id="email" name="email" placeholder="Ihre Email" required/><br><br>
+    <input type="email" id="email" name="email" placeholder="Ihre Email" /><br><br>
 
     <label for="nachricht">Was Sie uns Mitteilen möchten:</label><br><br>
-    <textarea id="nachricht" name="nachricht" rows="4" cols="50" placeholder="Was möchten Sie uns Mitteilen?" required></textarea><br><br>
+    <textarea id="nachricht" name="nachricht" rows="4" cols="50" placeholder="Was möchten Sie uns Mitteilen?" ></textarea><br><br>
 
     <p>Durch den Klick auf "Senden" werden Ihre Daten an unseren Kommandanten per E-Mail gesendet.</p>
 
     <input type="submit" value="absenden" name="senden" />
+    <input type="submit" value="zurück zur Startseite" name="zurueck" />
 
 </form>
 
