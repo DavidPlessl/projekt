15276f17ec7b6495ccde01874b7cdb1d3b6716e5
Sante1007 <santner.dominik1@hakspittal.at>
2024-03-04 19:59:03 +0100

    Mitglieder werden mit Bild in Datenbank gespeichert, und mit Bild ausgegeben

--- mitglieder.php
+++ mitglieder.php
@@ -1,38 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Startseite</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <style>
-    .bg-primary {
-    background-color: #ff0000 !important;
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mitglieder anzeigen</title>
+    <style>
+        body {
+            text-align: center;
+            margin-bottom: 70px; /* Füge einen unteren Abstand ein, um Platz für den Footer zu schaffen */
         }
 
-    .black-text {
-            color: black;
+        .mitglied-container {
+            max-width: 300px;
+            margin: auto;
+            margin-top: 30px;
+            display: flex;
+            flex-direction: column; /* Ändere die Anordnung auf Spaltenrichtung */
+            align-items: center; /* Zentriere die Elemente horizontal */
+            text-align: center;
         }
 
-        table {
-            width: 90%; /* Anpassen der Breite der Tabelle nach Bedarf */
-            margin: auto; /* Automatisches Zentrieren der Tabelle */
-            border-collapse: collapse; /* Zusammenführen von Zellengrenzen */
-            margin-bottom: 20px; /* abstand unten*/
-            margin-top: 20px; /*abstan oben*/
-        }
-
-        th {
-            text-align: center; /* Zentrieren des Texts in den Überschriftenzellen */
-            background-color: #c6e2ff; /* Hintergrundfarbe für Überschriftenzellen */
-            padding: 10px; /* Hinzufügen von Padding für bessere Lesbarkeit */
-        }
-
-        td {
-            /* Stil für Datenzellen hier anpassen, falls erforderlich */
-            padding: 10px;
-            border: 3px solid #ddd;
+        .mitglied-container img {
+            max-width: 100%;
+            max-height: 250px; /* Setze die maximale Höhe für die Bilder */
+            border-radius: 20px;
+            margin-bottom: 10px; /* Füge einen Abstand zwischen den Bildern hinzu */
         }
 
         footer {
@@ -40,131 +33,151 @@
             color: white;
             padding: 10px;
             text-align: center;
+            position: relative; /* Ändere die Position auf "relative" */
+            width: 100%;
+            bottom: 0;
         }
-        
+
         .navbar-form {
             display: flex;
             align-items: center;
-            margin-right: 10px;
+            margin-right: 2px;
         }
 
-  </style>
-</head>
-<body>
-<div class="p-4 bg-primary black-text text-center">
-   <h2>Hier finden sie einige unserer Mitglieder</h2><hr>
-</div>
+        .hintergrund {
+            background-color: red;
+            color: white;
+            padding: 20px;
+            text-align: center;
+        }
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <div class="container-fluid">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="startseite.php">zurück zur Startseite </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="aktivitaeten.php">Aktivitäten</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="geraete.php">Ausrüstung</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="werde_mitglied.php">werde Mitglied!</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="anmelden.php">Anmelden</a>
-        </li>
-      </ul>
-    </div>
-    <form class="navbar-form">
-        <div class="input-group search-input">
-          <span class="input-group-text">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a7.5 7.5 0 1 0-1.397 1.397h0a7.5 7.5 0 0 0 1.397-1.397zM13 7.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-          </span>
-          <input type="text" class="form-control" placeholder="Suche..." aria-label="Search" id="searchInput">
-        </div>
-      </form>
-    </div>
-</nav>
+        .mitglied-container h3,
+        .mitglied-container p {
+            text-align: center;
+        }
+    </style>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+
+<body>
 
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-    require_once('dbconnection.php');
+    try {
+        require_once('dbconnection.php');
 
-try {
-    $statement = $pdo->prepare("SELECT * FROM mitglieder");
-
-    $statement->execute();
-
-    echo "<table>";
-
-    if ($statement->rowCount() > 0) {
-        while ($zeile = $statement->fetch()) {
-                echo "<tr>" .
-                    //Funktion (Kommandant) noch hinzufügen zur DB, melden Datei und hier
-                    "<th>FW-Nummer</th>" . "<th>Vorname</th>" . "<th>Nachname</th>" . "<th>E-Mail</th>" .
-                    "</tr>" . 
-                    "<tr style='text-align:center'>" .
-                    "<td>" . $zeile['fw_nr'] . "</td>" .
-                    "<td>" . $zeile['vorname'] . "</td>" .
-                    "<td>" . $zeile['nachname'] . "</td>".
-                    "<td>" . $zeile['email'] . "</td>" .
-                    "</tr>";
-        }
+        $stmt = $pdo->query("SELECT dienstgrad, vorname, nachname, bild_pfad FROM mitglieder");
+        $mitglieder = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die("Datenbankfehler: " . $e->getMessage());
     }
 
-    echo "</table>";
-} catch (PDOException $ex) {
-    die("Fehler beim Ausgeben der Daten in die Datenbank!");
-}
+    ?>
 
-?>
+    <div class="hintergrund p-4">
+        <h2>Hier finden Sie einige unserer Mitglieder</h2>
+        <hr>
+    </div>
 
-<footer>
-    <form method="POST" action="anmelden.php">
-      <hr>
-      <h2>Hier finden Sie uns auf Social Media</h2><br>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="startseite.php">zurück zur Startseite</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="aktivitaeten.php">Aktivitäten</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="geraete.php">Ausrüstung</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="werde_mitglied.php">werde Mitglied!</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="anmelden.php">Anmelden</a>
+                </li>
+            </ul>
+            <form class="navbar-form">
+                <div class="input-group search-input">
+                    <span class="input-group-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-search" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a7.5 7.5 0 1 0-1.397 1.397h0a7.5 7.5 0 0 0 1.397-1.397zM13 7.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                    </span>
+                    <input type="text" class="form-control" placeholder="Suche..." aria-label="Search"
+                        id="searchInput">
+                </div>
+            </form>
+        </div>
+    </nav>
 
-      <div class="row">
-      <div class="col-lg">
-      <a href="https://www.instagram.com/ihre_instagram_seite" target="_blank">
-      <img src="images/instagram.jpg" alt="Instagram-Logo" width="50" height="50" style="border-radius: 50%;">
-      </a><br><br>
-      <p>Instagram</p>
-      </div>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($mitglieder as $mitglied) : ?>
+                <div class="mitglied-container col-lg-4 mb-4">
+                    <h3><?php echo $mitglied['dienstgrad']; ?></h3>
+                    <p><?php echo $mitglied['nachname']  . ' ' . $mitglied['vorname']; ?></p>
+                    <img src="<?php echo $mitglied['bild_pfad']; ?>" alt="Mitgliedsbild" class="mb-3">
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
-      <div class="col-lg">
-      <a href="https://www.flickr.com/photos/IhrFlickrAccount" target="_blank" style="border-radius: 10px; overflow: hidden; display: inline-block;">
-      <img src="images/flickr.jpeg" alt="Flickr-Logo" width="50" height="50" style="border-radius: 50%;">
-      </a><br><br>
-      <p>Flickr</p>
-      </div>
+    <footer>
+        <form method="POST" action="anmelden.php">
+            <hr>
+            <h2>Hier finden Sie uns auf Social Media</h2><br>
 
-      <div class="col-lg">
-      <a href="https://www.facebook.com/IhreFacebookSeite" target="_blank" style="border-radius: 10px; overflow: hidden; display: inline-block;">
-      <img src="images/facebook.jpeg" alt="Facebook-Logo" width="50" height="50" style="border-radius: 50%;">
-      </a><br><br>
-      <p>Facebook</p>
-      </div>
-      </div>
+            <div class="row">
+                <div class="col-lg">
+                    <a href="https://www.instagram.com/ihre_instagram_seite" target="_blank">
+                        <img src="images/instagram.jpg" alt="Instagram-Logo" width="50" height="50"
+                            style="border-radius: 50%;">
+                    </a><br><br>
+                    <p>Instagram</p>
+                </div>
 
-    </form>
-  </footer>
+                <div class="col-lg">
+                    <a href="https://www.flickr.com/photos/IhrFlickrAccount" target="_blank"
+                        style="border-radius: 10px; overflow: hidden; display: inline-block;">
+                        <img src="images/flickr.jpeg" alt="Flickr-Logo" width="50" height="50"
+                            style="border-radius: 50%;">
+                    </a><br><br>
+                    <p>Flickr</p>
+                </div>
 
-  <script>
+                <div class="col-lg">
+                    <a href="https://www.facebook.com/IhreFacebookSeite" target="_blank"
+                        style="border-radius: 10px; overflow: hidden; display: inline-block;">
+                        <img src="images/facebook.jpeg" alt="Facebook-Logo" width="50" height="50"
+                            style="border-radius: 50%;">
+                    </a><br><br>
+                    <p>Facebook</p>
+                </div>
+            </div>
+        </form>
+    </footer>
 
-   document.getElementById('searchInput').addEventListener('input', function () {
-      var searchValue = this.value.toLowerCase();
-      var rows = document.querySelectorAll('table tr');
-      rows.forEach(function (row) {
-        var shouldShow = Array.from(row.children).some(function (cell) {
-          return cell.textContent.toLowerCase().includes(searchValue);
+    <script>
+        document.getElementById('searchInput').addEventListener('input', function () {
+            var searchValue = this.value.toLowerCase();
+            var containers = document.querySelectorAll('.mitglied-container');
+            containers.forEach(function (container) {
+                var shouldShow = Array.from(container.children).some(function (element) {
+                    return element.textContent.toLowerCase().includes(searchValue);
+                });
+                container.style.display = shouldShow ? '' : 'none';
+            });
         });
-        row.style.display = shouldShow ? '' : 'none';
-      });
-    });
-
     </script>
+
 </body>
+
 </html>
--- registrieren.php
+++ registrieren.php
@@ -1,111 +1,139 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_POST['registrieren'])) {
+    if (empty($_POST['vname']) || empty($_POST['nname']) || empty($_POST['email']) || empty($_POST['nummer']) || empty($_POST['passwort']) || empty($_POST['passwortwh']) || empty($_POST['dienstgrad'])) {
+        echo "Bitte füllen Sie alle Felder!";
+    } else {
+        $vname = $_POST['vname'];
+        $nname = $_POST['nname'];
+        $email = $_POST['email'];
+        $nummer = $_POST['nummer'];
+        $passwort = $_POST['passwort'];
+        $passwortwh = $_POST['passwortwh'];
+        $dienstgrad = $_POST['dienstgrad'];
+        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+
+        if ($passwort != $passwortwh) {
+            echo "Die Passwörter stimmen nicht überein!";
+        } else {
+
+            try {
+                require_once('dbconnection.php');
+
+                $stmt = $pdo->prepare("SELECT fw_nr FROM mitglieder WHERE fw_nr = :fw_nr");
+
+                $stmt->bindParam(':fw_nr', $nummer);
+                $stmt->execute();
+
+                if ($stmt->rowCount() > 0) {
+                    echo "<i><b style='color: red'>Ihre gewünschte Feuerwehr-Nummer " . $nummer . " existiert bereits!<br> Bitte geben Sie eine andere Nummer ein!</b></i>";
+                } else {
+
+                    // Bildverarbeitung
+                    if (isset($_FILES['bild']) && $_FILES['bild']['error'] === UPLOAD_ERR_OK) {
+                        $uploadDirectory = "uploads/";
+                        $uploadedFile = $_FILES['bild'];
+                        $fileName = $uploadedFile['name'];
+                        $tempFilePath = $uploadedFile['tmp_name'];
+                        $newFileName = uniqid('', true) . '_' . $fileName;
+                        $uploadPath = $uploadDirectory . $newFileName;
+
+                        if (move_uploaded_file($tempFilePath, $uploadPath)) {
+                            $statement = $pdo->prepare("INSERT INTO mitglieder (vorname, nachname, email, fw_nr, passwort, bild_pfad, dienstgrad) VALUES (:vname, :nname, :email, :nummer, :passwort, :bild_pfad, :dienstgrad)");
+
+                            $statement->bindParam(':vname', $vname);
+                            $statement->bindParam(':nname', $nname);
+                            $statement->bindParam(':email', $email);
+                            $statement->bindParam(':nummer', $nummer);
+                            $statement->bindParam(':passwort', $passwort_hash);
+                            $statement->bindParam(':bild_pfad', $uploadPath);
+                            $statement->bindParam(':dienstgrad', $dienstgrad);
+
+                            $statement->execute();
+                            echo "Registrierung erfolgreich!";
+
+                            $pfad = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/anmelden.php';
+                            header('Location: ' . $pfad);
+                        } else {
+                            echo "Fehler beim Hochladen des Bildes.";
+                        }
+                    } else {
+                        echo "<h3>Bitte wählen Sie ein Bild aus.</h3>";
+                    }
+                }
+            } catch (PDOException $e) {
+                die("Registrierung fehlgeschlagen! Datenbankfehler: " . $e->getMessage());
+            }
+        }
+    }
+} else if (isset($_POST['zurueck'])) {
+
+    //weiterleiten
+    $pfad1 = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/startseite.php';
+    header('Location: ' . $pfad1);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrieren</title> 
+    <title>Registrieren</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="style_registrieren.css">
 </head>
+
 <body>
 
-    <?php
-    
-    if (isset($_POST['registrieren'])) {
-        if (empty($_POST['vname']) || empty($_POST['nname']) || empty($_POST['email']) || empty($_POST['nummer']) || empty($_POST['passwort']) || empty($_POST['passwortwh'])) {
-            echo "Bitte füllen Sie alle Felder!";
-        } else {
-            $vname = $_POST['vname'];
-            $nname = $_POST['nname'];
-            $email = $_POST['email'];
-            $nummer = $_POST['nummer'];
-            $passwort = $_POST['passwort'];
-            $passwortwh = $_POST['passwortwh'];
-            $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
-
-            if ($passwort != $passwortwh) {
-                echo "Die Passwörter stimmen nicht überein!";
-            } else {
-
-                try {
-                    require_once('dbconnection.php');
-
-                    $stmt = $pdo->prepare("SELECT fw_nr FROM mitglieder WHERE fw_nr = :fw_nr");
-
-                    $stmt->bindParam(':fw_nr', $nummer);
-                    $stmt->execute();
-
-                    if ($stmt->rowCount() > 0) {
-                        echo "<i><b style='color: red'>Ihre gewünschte Feuerwehr-Nummer ". $nummer . " existiert bereits!<br> Bitte geben Sie eine andere Nummer ein!</b></i>";
-                    } else {
-
-                        $statement = $pdo->prepare("INSERT INTO mitglieder (vorname, nachname, email, fw_nr, passwort) VALUES (:vname, :nname, :email, :nummer, :passwort)");
-
-                        $statement->bindParam(':vname', $vname);
-                        $statement->bindParam(':nname', $nname);
-                        $statement->bindParam(':email', $email);
-                        $statement->bindParam(':nummer', $nummer);
-                        $statement->bindParam(':passwort', $passwort_hash);
-
-                        $statement->execute();
-                        echo "Registrierung erfolgreich!";
-
-                        $pfad = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/anmelden.php';
-                        header('Location: ' . $pfad);
-                        
-                    }
-
-                } catch (PDOException $e) {
-                    die("Registrierung fehlgeschlagen!");
-                }
-
-            }
-
-        }
-
-    }else if (isset($_POST['zurueck'])) {
-
-        //weiterleiten
-        $pfad1 = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/startseite.php';
-        header('Location: ' . $pfad1);
-    }
-
-    ?>
     <div class="login">
-    <h1>Registrieren</h1>
+        <h1>Registrieren</h1>
 
-    <p>Bitte füllen Sie folgende Felder aus, um Ihren Account zu erstellen!<br>
-    Sollten Sie schon einen Account haben, hier <a href="anmelden.php">anmelden</a>.</p>
+        <p>Bitte füllen Sie folgende Felder aus, um Ihren Account zu erstellen!<br>
+            Sollten Sie schon einen Account haben, hier <a href="anmelden.php">anmelden</a>.</p>
 
-    <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+        <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" enctype="multipart/form-data">
 
-    <fieldset>
-            <legend>Anmeldedaten</legend>
-            <label for="vname">Vorname:</label>
-            <input type="text" name="vname" id="vname" placeholder="Max"><br><br>
+            <fieldset>
+                <legend>Anmeldedaten</legend>
+                <label for="vname">Vorname:</label>
+                <input type="text" name="vname" id="vname" placeholder="Max"><br><br>
 
-            <label for="nname">Nachname:</label>
-            <input type="text" name="nname" id="nname" placeholder="Musterman"><br><br>
+                <label for="nname">Nachname:</label>
+                <input type="text" name="nname" id="nname" placeholder="Musterman"><br><br>
 
-            <label for="email">E-Mail:</label>
-            <input type="email" name="email" id="email" placeholder="musterman@gmx.at"><br><br>
+                <label for="email">E-Mail:</label>
+                <input type="email" name="email" id="email" placeholder="musterman@gmx.at"><br><br>
 
-            <label for="nummer">FW-Nummer:</label>
-            <input type="number" name="nummer" id="nummer" placeholder="z. B. 25"><br><br>
+                <label for="nummer">FW-Nummer:</label>
+                <input type="number" name="nummer" id="nummer" placeholder="z. B. 25"><br><br>
 
-            <label for="passwort">Passwort:</label>
-            <input type="password" name="passwort" id="passwort" placeholder="min. 4 Zeichen"><br><br>
+                <label for="dienstgrad">Dienstgrad:</label>
+                <input type="text" name="dienstgrad" id="dienstgrad" placeholder="z. B. Hauptfeuerwehrmann"><br><br>
 
-            <label for="passwortwh">Passwort (Wiederholt):</label>
-            <input type="password" name="passwortwh" id="passwortwh" placeholder="min. 4 Zeichen"><br>
-    </fieldset> 
-    
-        <br>
-        <input type="submit" value="Registrieren" name="registrieren">       
-        <input type="submit" value="Zurück zur Startseite" name="zurueck" />
-    </form>
+                <label for="passwort">Passwort:</label>
+                <input type="password" name="passwort" id="passwort" placeholder="min. 3 Zeichen"><br><br>
+
+                <label for="passwortwh">Passwort (Wiederholt):</label>
+                <input type="password" name="passwortwh" id="passwortwh" placeholder="min. 3 Zeichen"><br>
+
+                <label for="bild">Bild hochladen:</label>
+                <input type="file" name="bild" id="bild"><br>
+            </fieldset>
+
+            <br>
+            <input type="submit" value="Registrieren" name="registrieren">
+            <input type="submit" value="Zurück zur Startseite" name="zurueck" />
+        </form>
     </div>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" rel="stylesheet">
 
 </body>
+
 </html>
